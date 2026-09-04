ab1d03e45ae42a9bfa670ec87dd456d33bfc713c
unknown <j.masson@SIO.local>
2026-09-04 11:58:06 +0200

    Amelioration syle navbar (modernisation)

--- component/navbar.php
+++ component/navbar.php
@@ -1,44 +1,18 @@
 <?php
 session_start();
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top shadow">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">
-                <img src="/img/logo.png" alt="Logo" class="logo">
-            </a>
-            
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="/index.php">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/html/articles.php">Articles</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/html/QSN.php">À propos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/html/Contact.php">Contact</a>
-                    </li>
-                    <?php
-                    if (isset($_SESSION["user_name"])) {
-                        echo '<li class="nav-item"><a class="nav-link" href="/html/profile.php">Profil</a></li>';
-                        echo '<li class="nav-item"><a class="nav-link btn btn-outline-light ms-2 px-3" href="/interact/logout.php">Déconnexion</a></li>';
-                    } else {
-                        echo '<li class="nav-item">
-                            <a class="nav-link btn btn-outline-light ms-2 px-3" href="/html/Inscription.php">Inscription</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link btn btn-outline-light ms-2 px-3" href="/html/Connexion.php">Connexion</a>
-                        </li>';
-                    }
-                    ?>
-                </ul>
-            </div>
-        </div>
+
+
+<header class="site-header">
+    <div class="logo"><img src="img/logo.png" alt="Logo"></div>
+    <nav class="nav-menu">
+        <ul>
+            <li><a href="#home">Accueil</a></li>
+            <li><a href="#services">Articles</a></li>
+            <li><a href="#contact">À propos</a></li>
+        </ul>
     </nav>
+    <div class="header-actions">
+        <button class="btn-cta">Connexion</button>
+    </div>
+</header>
